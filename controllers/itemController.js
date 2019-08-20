8438b40fac99c762fe955ddc5c545230c1e302c3
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2019-08-20 16:44:29 -0600

    call items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,8 +1,8 @@
-import ItemService from "../services/itemService.js"
+import ItemService from "../services/itemService.js";
 
 let _itemService = new ItemService()
 
-function _drawItem() {
+function _draw() {
   let item = _itemService.Item
   let item2 = _itemService.Item2
   let item3 = _itemService.Item3
@@ -10,6 +10,10 @@ function _drawItem() {
   let itemNameElement = document.querySelector("#sensu")
   let item2NameElement = document.querySelector("#dragon-balls")
   let item3NameElement = document.querySelector("#fusion-dance")
+
+  itemNameElement.textContent = item.name
+  item2NameElement.textContent = item2.name
+  item3NameElement.textContent = item3.name
 
 
 }
@@ -23,6 +27,6 @@ function _drawItem() {
 export default class ItemController {
   constructor() {
     console.log("How does this thing work?")
-    _drawItem()
+    _draw()
   }
 }
--- slapgame2.js
+++ slapgame2.js
@@ -12,39 +12,3 @@ class Slapgame2 {
 }
 
 window["slapgame2"] = new Slapgame2()
-
-//items to restore health
-function giveSensu() {
-  if (fighter.goku.health < fighter.frieza.health) {
-    fighter.goku.health += items.sensu.modifier;
-    fighter.goku.items.push(items.sensu)
-    alert(items.sensu.description)
-  } else if (fighter.frieza.health < fighter.goku.health) {
-    fighter.frieza.health += items.sensu.modifier;
-    fighter.frieza.items.push(items.sensu)
-    alert(items.sensu.description)
-  }
-  negatives()
-  update()
-}
-
-function giveDragonBalls() {
-  if (fighter.goku.health < fighter.frieza.health) {
-    fighter.goku.health += items.dragonBalls.modifier;
-    fighter.goku.items.push(items.dragonBalls)
-    alert(items.dragonBalls.description)
-  }
-  negatives()
-  update()
-}
-
-function fusion() {
-  if (fighter.goku.health < fighter.frieza.health) {
-    fighter.goku.attacks.kamehameha += 15;
-    fighter.goku.health += items.fusion.modifier;
-    fighter.goku.items.push(items.fusion)
-    alert(items.fusion.description)
-  }
-  negatives()
-  update()
-}
